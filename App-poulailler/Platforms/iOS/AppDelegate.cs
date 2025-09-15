53c1296663e37cc1085e5617fb40ebe656fef853
SLAB29 <SLAB29@KM103-03-M>
2025-09-15 08:26:17 +0200

    Ajoutez des fichiers projet.

---
+++ App-poulailler/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace App_poulailler
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
