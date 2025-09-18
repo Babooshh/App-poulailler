6472b7c15377a0361593a38ac49293d5a7251d98
HugoMetais <hugo.metais@saintmichelannecy.fr>
2025-09-18 12:52:55 +0200

    colors

--- App-poulailler/MainPage.xaml.cs
+++ App-poulailler/MainPage.xaml.cs
@@ -11,13 +11,20 @@
 
         private void OnInterrupteurToggled(object? sender, ToggledEventArgs e)
         {
-            if (e.Value)
+            if (sender is Switch interrupteur)
             {
-                DisplayAlert("Action", "Ouvrir", "OK");
-            }
-            else
-            {
-                DisplayAlert("Action", "Fermer", "OK");
+                if (e.Value)
+                {
+                    interrupteur.ThumbColor = Colors.Green;
+                    interrupteur.OnColor = Colors.Green;
+                    DisplayAlert("Action", "Ouvrir", "OK");
+                }
+                else
+                {
+                    interrupteur.ThumbColor = Colors.Red;
+                    interrupteur.OnColor = Colors.Red;
+                    DisplayAlert("Action", "Fermer", "OK");
+                }
             }
         }
     }
